3bb4f6836532ea55c32d10dda789d0ea5ed1e200
Efornies <eleforbel@gmail.com>
2022-03-28 11:38:24 +0000

    nuevos cambios

--- src/app.js
+++ src/app.js
@@ -4,5 +4,25 @@ import "./style.css";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello ");
 };
+
+//Definir
+
+let palos = ["♦", "♥", "♠", "♣"];
+let numeros = [
+  "A",
+  "2",
+  "3",
+  "4",
+  "5",
+  "6",
+  "7",
+  "8",
+  "9",
+  "10",
+  "J",
+  "Q",
+  "K"
+];
+
+//fórmulas
